5bef876e4a6fccd5f8630acc9454cd2d12b18c75
Hugh <hgberryman8@gmail.com>
2024-01-09 22:20:55 -0700

    link outs to sureapp storybook, chromatic, and github repo I've created as POCs for the design technologist application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@
 
 import React from "react";
 import Image from "next/image";
+
 import { Button } from "@/stories/Button";
 
 export default function SureAppDlsLandingPage() {
+  const handleClick = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <section className="flex h-screen flex-col items-center justify-center space-y-36">
       <header className="flex flex-col items-center space-y-3">
@@ -17,8 +22,33 @@ export default function SureAppDlsLandingPage() {
         />
         <h1 className="font-semibold text-xl md:text-3xl">SureApp DLS</h1>
       </header>
-      <main>
-        <Button label="View SureApp's Storybook" primary />
+      <main className="flex flex-col items-center justify-center space-y-2">
+        <div className="space-x-2">
+          <Button
+            label="Storybook"
+            primary
+            onClick={() =>
+              handleClick(
+                "https://659e1a04f5eed59ea7345e35-mlxwvuvkbj.chromatic.com/?path=/docs/configure-your-project--docs"
+              )
+            }
+          />
+          <Button
+            label="Chromatic"
+            primary={false}
+            onClick={() =>
+              handleClick(
+                "https://www.chromatic.com/library?appId=659e1a04f5eed59ea7345e35&inviteToken=chpi_6f14430c7f3b4d01ac1234969700d56f"
+              )
+            }
+          />
+        </div>
+        <Button
+          label="GitHub"
+          onClick={() =>
+            handleClick("https://github.com/HughBerriez/sure-app-dls")
+          }
+        />
       </main>
       <footer className="flex flex-col justify-center items-center space-y-3 text-center">
         <p className="font-medium text-sm">
