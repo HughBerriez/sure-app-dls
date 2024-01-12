2c21ad419e655fb8bac46c75f7f0bac37e1175c2
Hugh <hgberryman8@gmail.com>
2024-01-11 18:13:47 -0700

    abstract out header component

--- src/app/Triangles/page.tsx
+++ src/app/Triangles/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import Header from "@/components/Header";
 
 export default function TrianglesPage() {
-  return <div>TrianglesPage</div>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
