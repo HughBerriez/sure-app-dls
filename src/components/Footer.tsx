73468ea4b0abf5228efb6f5fa1147942402ef5d2
Hugh <hgberryman8@gmail.com>
2024-01-11 19:48:11 -0700

    cleanup tailwind config file. Tweaks to footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,9 @@ export default function Footer() {
   return (
     <footer className="flex flex-row justify-between items-center px-8 border-b-[1px] border-b-slate-300">
       <Logo theme="light" />
+      <div className="text-white text-sm font-normal font-['Roboto'] leading-tight">
+        © 2023 Triangles, Inc.
+      </div>
 
       <div>
         <p className="text-slate-300 text-sm font-medium leading-5">
