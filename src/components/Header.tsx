f154f3f1fb9af9110f7dd7dea552de1e63647d80
Hugh <hgberryman8@gmail.com>
2024-01-11 23:31:14 -0700

    search input styling fixes for better focus accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,41 @@
+"use client";
+
+import React, { useState, useRef } from "react";
 import Image from "next/image";
 import Logo from "./Logo";
 
 export default function Header() {
+  const [isInputFocused, setInputFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null); // Create a ref for the input
   const inputBg = "bg-white";
+
+  const focusInput = () => {
+    inputRef.current?.focus(); // Focus the input when the container is clicked
+  };
 
   return (
     <header className="flex flex-row justify-between items-center px-8 border-b-[1px] border-b-slate-300 py-3">
       <Logo theme="dark" />
 
       <div
-        className={`flex flex-row border-[1px] rounded border-slate-300 px-5 py-3 ${inputBg}`}
+        className={`flex flex-row items-center rounded border-[1px] px-5 py-3 ${
+          isInputFocused ? "ring-2 ring-brand-light" : "border-slate-300"
+        } ${inputBg}`}
+        onClick={focusInput}
       >
         <Image
           src="/magnifying-glass-outline.svg"
-          alt="Triangles logo-mark"
+          alt="Search icon"
           width={24}
           height={24}
         />
-        <input placeholder="Search" className={inputBg} />
+        <input
+          ref={inputRef}
+          placeholder="Search"
+          className={`px-2 ${inputBg} focus:outline-none`}
+          onFocus={() => setInputFocused(true)}
+          onBlur={() => setInputFocused(false)}
+        />
       </div>
     </header>
   );
