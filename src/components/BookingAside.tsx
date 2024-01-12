ec7088eb9c548f3e6d43e1cd5a8c67f52a73f7b2
Hugh <hgberryman8@gmail.com>
2024-01-11 22:33:38 -0700

    responsive HeroListing work. booking info component work

--- src/components/BookingAside.tsx
+++ src/components/BookingAside.tsx
@@ -1,9 +1,47 @@
+import { Button } from "@/stories/sure-examples/Button";
+import Link from "next/link";
 import React from "react";
 
 export default function BookingAside() {
   return (
-    <div>
-      <input type="date" />
+    <div className="flex flex-col shadow-lg h-auto p-4 space-y-6">
+      <div>
+        <div className="text-base font-bold">Summary</div>
+        <div className="flex flex-row justify-between">
+          <div className="text-3xl font-bold">$200.00</div>
+          <div className="w-12 h-5 text-neutral-400 text-sm font-bold">
+            per day
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <label className="text-sm font-bold">Check In</label>
+        <input type="date" />
+        <label className="text-sm font-bold">Check Out</label>
+        <input type="date" />
+        <label className="text-sm font-bold">Guests</label>
+        <input type="date" />
+      </div>
+
+      <div className="space-y-4">
+        <div>
+          <div className="flex flex-row justify-between">
+            <div className="text-sm font-normal">Service Fee</div>
+            <div className="text-sm font-normal">$30</div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="text-sm font-bold">Total</div>
+            <div className="text-sm font-bold">$300</div>
+          </div>
+        </div>
+        <Link
+          href="#"
+          className="bg-brand hover:bg-brand-dark active:bg-brand-light text-white font-bold py-2 px-4 rounded"
+        >
+          Book now
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/HeroListing.tsx
+++ src/components/HeroListing.tsx
@@ -4,34 +4,39 @@ import Ratings from "./Ratings";
 
 export default function HeroListing() {
   return (
-    <div className="space-y-7">
-      <div className="flex flex-row justify-between items-end">
-        <div>
-          <div className="text-3xl font-semibold">Private Penthouse</div>
-          <Ratings numReviews={200} rating={4} />
-        </div>
-        <div className="flex flex-row items-center space-x-2">
-          <Image
-            src={"/map-pin-outline.svg"}
-            alt="map-pin-outline.svg"
-            width={16}
-            height={16}
-          />
-          <div className="text-sm font-medium">
-            Beverly Hills, California, United States
+    <div className="space-y-7 max-w-[600px]">
+      <div className="space-y-4">
+        <div className="flex flex-row justify-between items-end">
+          <div className="space-y-2">
+            <div className="text-3xl font-semibold">Private Penthouse</div>
+            <Ratings numReviews={200} rating={4} />
+          </div>
+          <div className="flex flex-row items-center space-x-2">
+            <Image
+              src={"/map-pin-outline.svg"}
+              alt="map-pin-outline.svg"
+              width={16}
+              height={16}
+            />
+            <div className="text-sm font-medium">
+              Beverly Hills, California, United States
+            </div>
           </div>
         </div>
+
+        <Image
+          src={"/penthouse.jpeg"}
+          alt="Penthouse listing"
+          className="rounded"
+          layout="responsive"
+          width={600}
+          height={400}
+        />
       </div>
-      <Image
-        src={"/penthouse.jpeg"}
-        alt="Penthouse listing"
-        width={600}
-        height={400}
-      />
 
       <div className="space-y-4">
         <div className="text-2xl font-semibold">Description</div>
-        <div className="w-96 text-black text-base font-normal leading-normal">
+        <div className="text-base">
           Elevate your lifestyle with this stunning penthouse in the heart of
           Beverly Hills. Offering awe-inspiring panoramic views, a spacious open
           layout, a gourmet kitchen, and a private terrace, this penthouse is
